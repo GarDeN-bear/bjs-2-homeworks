2878517fa31d0e81b5511ac8f28e073ab91c8d20
Denis Garpinich <d.garpinich@wisetech.pro>
2025-01-17 18:29:54 +0300

    Полностью выполненные задания

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,21 +1,24 @@
 'use strict'
 function solveEquation(a, b, c) {
   let arr = [];
-  const D = b ** 2 - 4 * a * c;
-  if (D == 0) {
+  const discriminant = b ** 2 - 4 * a * c;
+  if (discriminant == 0) {
     arr.push(-b / (2 * a));
-  } else if (D > 0) {
-    const Dsqrt = D ** 0.5;
-    arr.push((-b + Dsqrt) / (2 * a), (-b - Dsqrt) / (2 * a));
+  } else if (discriminant > 0) {
+    const discriminantSqrt = discriminant ** 0.5;
+    arr.push(
+        (-b + discriminantSqrt) / (2 * a), (-b - discriminantSqrt) / (2 * a));
   }
 
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  const P = percent / 100 / 12;
-  const S = amount - contribution;
-  const pay = S * (P + (P / (((1 + P) ** countMonths) - 1)));
+  const convertedPercent = percent / 100 / 12;
+  const loanBody = amount - contribution;
+  const pay = loanBody *
+      (convertedPercent +
+       (convertedPercent / (((1 + convertedPercent) ** countMonths) - 1)));
   const totalPay = pay * countMonths;
   return parseFloat(totalPay.toFixed(2));
 }
