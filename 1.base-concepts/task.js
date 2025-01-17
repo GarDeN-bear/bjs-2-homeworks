b652deba2d810b758f0d6325a422859c1e925dd9
Denis Garpinich <d.garpinich@wisetech.pro>
2025-01-17 16:54:16 +0300

    1 задание 1 лекции

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,15 @@
-"use strict"
+'use strict'
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let D = b ** 2 - 4 * a * c;
+  if (D == 0) {
+    arr.push(-b / (2 * a));
+  } else if (D > 0) {
+    let Dsqrt = D ** 0.5;
+    arr.push((-b + Dsqrt) / (2 * a), (-b - Dsqrt) / (2 * a));
+  }
+
   return arr;
 }
 
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
-}
+function calculateTotalMortgage(percent, contribution, amount, countMonths) {}
