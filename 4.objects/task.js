114e641ae6b00e1b981270b259687f60721b4559
Denis Garpinich <d.garpinich@wisetech.pro>
2025-01-22 19:52:39 +0300

    [200~»

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,35 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  
+Student.prototype.setSubject =
+    function(subjectName) {
+  this.subjectName = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+    Student.prototype.addMarks =
+        function(...marks) {
+  if (this.isExcuded == true) {
+    retrurn;
+  }
+  this.marks.push(...marks);
 }
 
-Student.prototype.getAverage = function () {
-  
+        Student.prototype.getAverage =
+            function() {
+  let avg = 0;
+  if (this.marks.length == 0) {
+    return avg;
+  }
+  avg = this.marks.reduce((accum, val) => accum + val, 0) / this.marks.length;
+  return avg;
 }
 
-Student.prototype.exclude = function (reason) {
-  
+            Student.prototype.exclude = function(reason) {
+  delete this.subjectName;
+  delete this.marks;
+  this.excluded = reason;
 }
