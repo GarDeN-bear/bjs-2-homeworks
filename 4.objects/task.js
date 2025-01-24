232d7de36f204e4cef7d60d94f75e5f6eef5010e
Denis Garpinich <d.garpinich@wisetech.pro>
2025-01-24 10:22:44 +0300

    Домашнее задание к лекции 4 «Объекты» полностью выполнено

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -5,32 +5,34 @@ function Student(name, gender, age) {
   this.marks = [];
 }
 
-Student.prototype.setSubject =
-    function(subjectName) {
-  this.subjectName = subjectName;
-}
+Student.prototype.setSubject = function(subjectName) {
+  this.subject = subjectName;
+};
 
-    Student.prototype.addMarks =
-        function(...marks) {
-  if (this.isExcuded == true) {
-    retrurn;
+Student.prototype.addMarks = function(...marks) {
+  if (!this.hasOwnProperty('marks')) {
+    return;
   }
 
   this.marks.push(...marks);
-}
+};
 
-        Student.prototype.getAverage =
-            function() {
+Student.prototype.getAverage = function() {
   let avg = 0;
+  if (!this.hasOwnProperty('marks')) {
+    return avg;
+  }
+
   if (this.marks.length == 0) {
     return avg;
   }
+
   avg = this.marks.reduce((accum, val) => accum + val, 0) / this.marks.length;
   return avg;
-}
+};
 
-            Student.prototype.exclude = function(reason) {
-  delete this.subjectName;
+Student.prototype.exclude = function(reason) {
+  delete this.subject;
   delete this.marks;
   this.excluded = reason;
-}
+};
