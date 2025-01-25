3c1af5c2e099fe164108c14c03e55c302f8d8df6
Denis Garpinich <d.garpinich@wisetech.pro>
2025-01-25 19:37:36 +0300

    Правки 4 задания

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -18,11 +18,7 @@ Student.prototype.addMarks = function(...marks) {
 };
 
 Student.prototype.getAverage = function() {
-  if (!this.hasOwnProperty('marks')) {
-    return 0;
-  }
-
-  if (this.marks.length === 0) {
+  if (!this.hasOwnProperty('marks') || (this.marks.length === 0)) {
     return 0;
   }
 
